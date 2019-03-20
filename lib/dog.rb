b26eae37c1fc4d468209676b048f6fa87934725c
Daniel Schneider <daschne8@gmail.com>
2019-03-20 13:20:17 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  attr_accessor
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+  def clear_all
+    @all = []
+  end
+  def self.all
+    @@all.each{|dog| puts dog.name}
+  end
+  
+end
